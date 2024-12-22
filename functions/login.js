7483d69b190b530774c187df23e32fc26405ccd1
Ratthabhumi <ratthaphumi@gmail.com>
2024-12-22 22:01:53 +0700

    index

--- functions/login.js
+++ functions/login.js
@@ -1,9 +1,6 @@
 const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 
-// Load environment variables from .env file
-require('dotenv').config();
-
 const uri = process.env.MONGODB_URI;
 
 exports.handler = async function(event, context) {
